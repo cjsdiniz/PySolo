43a8dd91eb863f71728efddf41240c25b045b09e
cjdiniz <cjsdiniz@outlook.com>
2021-06-26 20:01:10 +0100

    strings&variables

---
+++ strings&variables.py
@@ -0,0 +1,43 @@
+# String
+print("Python is fun!")
+print('Always look on the bright side of life')
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+print("Spam" + 'eggs')
+print("spam" * 3)
+print(4 * '2')
+user = "James"
+x = 7
+print(x)
+print(x + 3)
+print(x)
+spam = "eggs"
+print(spam * 3)
+
+# Variables
+x = 123.456
+print(x)
+
+x = "This is a string"
+print(x + "!")
+# Taking User Input
+name = input("Enter your name: ")
+print("Hello, " + name)
+# Working with Input
+age = int(input())
+print(age)
+x = "2"
+y = "4"
+z = int(x) + int(y)
+print(z)
+name = input()
+age = input()
+print(name + " is " + age)
+# In-Place and Walrus Operators
+x = 4
+x *= 3
+print(x)
+x = "a"
+x *= 3
+print(x)
+num = int(input())
+print(num)
