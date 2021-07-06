9ff419da8e09e026fe08aefe11169822378ed7ef
cjdiniz <cjsdiniz@outlook.com>
2021-07-06 22:43:05 +0100

    Update moretypes.py

--- moretypes.py
+++ moretypes.py
@@ -1,2 +1,23 @@
 # none
+# Ex1
 print(None)
+
+# Ex2
+
+
+def some_func():
+    print("Hi!")
+
+
+var = some_func()
+print(var)
+
+# Ex3
+foo = print()
+if foo == None:
+
+    print(1)
+
+else:
+
+    print(2)
