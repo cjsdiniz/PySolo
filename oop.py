c9824e1500d4a0c941d86f0b1c221403104f0988
cjdiniz <cjsdiniz@outlook.com>
2021-07-14 23:29:41 +0100

    Oop upd

--- oop.py
+++ oop.py
@@ -133,3 +133,23 @@ c = C()
 c.method()
 c.another_method()
 c.third_method()
+
+
+print("\nMagic Methods & Operator Overloading")
+print("\n#1")
+
+
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+
+first = Vector2D(5, 7)
+second = Vector2D(3, 9)
+result = first + second
+print(result.x)
+print(result.y)
